25b36f5329a3a47e743a0f0eb098e85dae6021a4
Normandie-RP-LIFE-SEMI-WL <james2001del@gmail.com>
2025-11-16 23:30:56 +0100

    Update ping.js

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
-  data: new SlashCommandBuilder().setName('ping').setDescription('Répond pong'),
-  async execute(interaction) {
-    await interaction.reply(`Pong — ${Math.round(interaction.client.ws.ping)}ms`);
-  }
+data: new SlashCommandBuilder().setName('ping').setDescription('Pong test'),
+async execute(i) {
+i.reply(`Pong: ${Math.round(i.client.ws.ping)}ms`);
+}
 };
